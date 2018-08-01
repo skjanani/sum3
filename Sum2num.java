24460a57a0785a27b63c73ecac4dce46158b959d
skjanani <41111202+skjanani@users.noreply.github.com>
2018-08-01 14:06:37 +0530

    Add files via upload

---
+++ Sum2num.java
@@ -0,0 +1,16 @@
+package janani;
+import java.util.Scanner;
+public class Sum2num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n,n1,sum;
+System.out.println("enter the number");
+Scanner ja=new Scanner(System.in);
+n=ja.nextInt();
+n1=ja.nextInt();
+sum=n+n1;
+System.out.println(sum);
+	}
+
+}
